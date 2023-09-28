0551c7bc230bc68ed9ba1be5d91898b949096c6f
hp <youssefrefa3ii.10@gmail.com>
2023-09-28 14:32:53 +0300

    finish radio_tab.dart

--- lib/tabs/radio_tab.dart
+++ lib/tabs/radio_tab.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/theme_data.dart';
 
 class RadioTab extends StatelessWidget {
   const RadioTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment:MainAxisAlignment.spaceEvenly,
         children: [
-          Image(
-            image: AssetImage('assets/images/radio_image.png',),
+          const Image(
+            image: AssetImage(
+              'assets/images/radio_image.png',
+            ),
+          ),
+
+          Text(
+            'إذاعة القرآن الكريم',
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium!
+                .copyWith(color: MyThemeData.secondColor),
+          ),
+
+          ImageIcon(
+            const AssetImage('assets/images/radio_icon.png',)
+            ,size: 150,color: MyThemeData.primaryColor,
           )
         ],
       ),
